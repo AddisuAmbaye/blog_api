d8810b3db231555283af6319249d79e1db41e083
addis <addeson3@gmail.com>
2023-03-07 21:22:52 -0800

    first commit

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
-
+require('dotenv').config();
+require('./config/dbConnect');
 const app = express();
 
 // middleware
-
 // routes
 // error handlers middleware
 // listen to server
+
+const PORT = process.env.PORT || 9000;
+app.listen(PORT, console.log(`server is up and listening on port ${PORT}`));
