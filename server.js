a49f25026ca9b73d5b02315e9e0b326c154f8a63
addis <addeson3@gmail.com>
2023-03-09 09:21:04 -0800

    updates

--- server.js
+++ server.js
@@ -1,90 +1,40 @@
-const express = require('express');
-require('dotenv').config();
-require('./config/dbConnect');
+const express = require("express");
+const globalErrHandler = require("./middlewares/globalErrHandler");
+const categoryRouter = require("./routes/categories/categoryRoutes");
+const commentRouter = require("./routes/comments/commentRoutes");
+const postRouter = require("./routes/posts/postRoutes");
+const userRouter = require("./routes/users/userRoutes");
+
+require("dotenv").config();
+require("./config/dbConnect");
+
 const app = express();
 
-// middleware
-// routes
+//middlewares
+app.use(express.json()); //pass incoming payload
 
+//routes
+//----
 //users route
-app.post('/api/v1/users/register', async() => {
-    try {
-        res.json({
-            status: 'sucess',
-            data: 'user registered',
-             });
-    } catch (error) {
-        res.json(error.message);
-    }
+app.use("/api/v1/users/", userRouter);
+//posts route
+app.use("/api/v1/posts", postRouter);
+//comments route
+app.use("/api/v1/comments", commentRouter);
+//categories route
+app.use("/api/v1/categories", categoryRouter);
+
+//Error handlers middleware
+app.use(globalErrHandler);
+
+//404 error
+app.use("*", (req, res) => {
+  console.log(req.originalUrl);
+  res.status(404).json({
+    message: `${req.originalUrl} - Route Not Found`,
+  });
 });
-
-//user login
-app.post('/api/v1/users/login', async() => {
-    try {
-        res.json({
-            status: 'sucess',
-            data: 'user login',
-             });
-    } catch (error) {
-        res.json(error.message);
-    }
-});
-
-//user id
-
-app.get('/api/v1/users/:id', async() => {
-    try {
-        res.json({
-            status: 'sucess',
-            data: 'profile route',
-             });
-    } catch (error) {
-        res.json(error.message);
-    }
-});
-
-//all users
-
-app.get('/api/v1/users', async() => {
-    try {
-        res.json({
-            status: 'sucess',
-            data: 'profile route',
-             });
-    } catch (error) {
-        res.json(error.message);
-    }
-});
-
-// delete
-
-app.delete('/api/v1/users/:id', async() => {
-    try {
-        res.json({
-            status: 'sucess',
-            data: 'delete user route',
-             });
-    } catch (error) {
-        res.json(error.message);
-    }
-});
-
-//update
-
-app.put('/api/v1/users/:id', async() => {
-    try {
-        res.json({
-            status: 'sucess',
-            data: 'update user',
-             });
-    } catch (error) {
-        res.json(error.message);
-    }
-});
-
-
-// error handlers middleware
-// listen to server
-
+//Listen to server
 const PORT = process.env.PORT || 9000;
-app.listen(PORT, console.log(`server is up and listening on port ${PORT}`));
+
+app.listen(PORT, console.log(`Server is up and running on ${PORT}`));
